a7f11b0ccea12d47b63c483614ca7780608ce084
HowTroublesome <justforthestyle@abv.bg>
2015-03-29 19:40:00 +0300

    Fixed week1 problems v2

--- week1/1-DebuggingFun/src/com/hackbulgaria/corejava/FaultyProblem8.java
+++ week1/1-DebuggingFun/src/com/hackbulgaria/corejava/FaultyProblem8.java
@@ -1,14 +1,15 @@
 package com.hackbulgaria.corejava;
 
 public class FaultyProblem8 {
+
 	public int getNumberOfDigits(int someNumber) {
-		int digitsCount = 0;
-		
-		while(someNumber != 0) {
-		    digitsCount++;
-			someNumber = someNumber / 10;
-		}
-		
-		return digitsCount;
+	    int digitsCount = 0;
+	    
+	    while(someNumber != 0) {
+	        digitsCount++;
+	        someNumber = someNumber / 10;
+	    }
+	    
+	    return digitsCount;
 	}
 }
